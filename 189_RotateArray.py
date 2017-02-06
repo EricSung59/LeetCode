e46e7db8851b3ae019b44bc07ed94ea9a29951be
EricSung59 <tianyus@umich.edu>
2017-02-06 14:32:45 -0500

    189_RotateArray

--- 189_RotateArray.py
+++ 189_RotateArray.py
@@ -5,11 +5,10 @@ class Solution(object):
         :type k: int
         :rtype: void Do not return anything, modify nums in-place instead.
         """
+        n = len(nums)
         k1 = k % len(nums)
-        nums = nums[-k1:] + nums[0:len(nums) - k1 - 1]
+        nums = nums[-k1:] + nums[:-k1]
         return nums
 
 p = Solution()
-print(p.rotate([1, 2, 3, 4, 5, 6, 7, 8], 3))
-
-# 反方向切片时也是从左到右【-3：-1】为-3，-2,。不包括-1
+print(p.rotate([1], 3))
